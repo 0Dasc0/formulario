517ec046a0539cabfaacb2e52697cc5888eaba8e
0Dasc0 <93265776+0Dasc0@users.noreply.github.com>
2021-11-10 09:52:44 -0500

    cambio

---
+++ ASSES/index.js
@@ -0,0 +1,9 @@
+function capturar (){
+  var nombre document.getElementById("nombres_").value;
+  var apellido document.getElementById("apellidos_").value;
+  var documento document.getElementById("documento_").value;
+  var correo document.getElementById("correo_").value;
+  var observacion document.getElementById("observaciones_").value;
+  console.console.log(nombre +" "+ apellido +" "+ documento +" "+ correo +" "+ observacion);
+
+}
